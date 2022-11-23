e2715ab3950687a2a89102b29e208701b85a587c
Bfunk54 <ben_fein@icloud.com>
2022-11-22 21:51:32 -0500

    added the last two relationships in the model index

--- models/index.js
+++ models/index.js
@@ -10,4 +10,20 @@ Category.hasMany(Product, {
   onDelete: "CASCADE",
 });
 
-module.exports = { Traveller, Location, Trip };
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'product_tags'
+});
+
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tag_products'
+});
+
+module.exports = { Category, Product };
