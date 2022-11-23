fb7589a20b4e621b65ed128fb58b830088bcd0d2
Bfunk54 <ben_fein@icloud.com>
2022-11-22 22:17:14 -0500

    added a get route to find all categories with their products

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,27 +1,32 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
-// The `/api/categories` endpoint
-
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+// Get route for all categories
+router.get("/", (req, res) => {
+  // find all categories and include its products
+  try {
+    Category.findAll({
+      include: [Product],
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // create a new category
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
 });
 
