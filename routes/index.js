a8e830987b0558f88c54f9bfc325834c77e5a80f
Bfunk54 <ben_fein@icloud.com>
2022-11-22 22:39:56 -0500

    added comments to index route

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
+// Require ezpress module for routing
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
+// Add prefix of `/api` to routes created in `api` directory
 router.use('/api', apiRoutes);
 
+// If no API routes are hit, send the a message telling the user they are in the wrong place
 router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>")
 });
